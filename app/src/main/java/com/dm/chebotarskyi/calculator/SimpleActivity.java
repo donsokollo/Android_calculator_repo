418bf14f454bc1f0b2d0f743a5f69d0aeb41a3fb
Dmytro Chebotarskyi <dm.chebotarskyi@gmail.com>
2017-03-16 11:27:23 -0700

    Initial commit

---
+++ app/src/main/java/com/dm/chebotarskyi/calculator/SimpleActivity.java
@@ -0,0 +1,13 @@
+package com.dm.chebotarskyi.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_calculator);
+    }
+}
